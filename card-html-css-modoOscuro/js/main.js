85276fc0941dd060290e4d0a6addc039c0dabf80
martinps206 <martinps_cc@hotmail.com>
2022-08-20 01:22:15 -0300

    Diseño modo oscuro

---
+++ card-html-css-modoOscuro/js/main.js
@@ -0,0 +1,16 @@
+let modo = document.getElementById("modo");
+let body = document.body;
+
+modo.addEventListener("click", function () {
+    let val = body.classList.toggle("dark");
+    localStorage.setItem("modo", val)
+})
+
+let valor = localStorage.getItem("modo");
+
+if (valor) {
+    body.classList.add("dark")
+} else {
+    body.classList.remove("dark")
+} 
+
